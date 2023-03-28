6a8c6f4ff5124180150d7ac8e3cfda08eb9a987f
Adil Ahmad <adilahmad17@users.noreply.github.com>
2023-03-28 14:08:48 -0700

    Update test2.c

--- user/test2.c
+++ user/test2.c
@@ -14,7 +14,6 @@
 /* Stack region for different threads */
 char stacks[PGSIZE*MAXULTHREADS];
 
-/* Simple example that allocates heap memory and accesses it. */
 void ul_start_func(void) {
     printf("[.] started the thread function (tid = %d) \n", get_current_tid());
 
